6f7027072c6e19ab69fc7b2616d40e0d69f1d169
Karius <kittarius@gmail.com>
2012-06-22 20:38:40 +0800

    Tools.Scan 函数根据传入路径参数字符串是否为unicode来具体处理

--- Tools.py
+++ Tools.py
@@ -51,7 +51,11 @@ def ScanDir (rootDir, filesort = False, level = None):
             break
 
     if filesort:
-        return sorted (fileList, key=str.lower)
+        # 根据传入rootDir是否为unicode类型来具体处理
+        if isinstance(rootDir,unicode):
+            return sorted (fileList, key=unicode.lower)
+        else:
+            return sorted (fileList, key=str.lower)
 
     return fileList
 
