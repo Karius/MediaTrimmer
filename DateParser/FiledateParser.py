1429d7df1be7121491c288d75a39e0512025e061
Sylar <karius@gmail.com>
2019-01-21 23:11:46 +0800

    重构中

---
+++ DateParser/FiledateParser.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .DateParser import DateParseManager
+from .DateParser import DateParser
+from datetime import date
+
+
+class FiledateParser(DateParser):
+    def __init__ (self):
+        pass
+    
+    def Date (self, filename):
+        
+        #date(year, month, day)
+        pass
+
+
+
+if __name__ != "__main__":
+    DateParseManager.RegisterParser ("FILEDATE", FiledateParser)
