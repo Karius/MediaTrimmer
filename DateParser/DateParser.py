9f7d64e11689925c5a2c3138891b86b658dbfe84
l <kittarius@gmail.com>
2019-02-02 12:28:35 +0800

    重构中。。。

--- DateParser/DateParser.py
+++ DateParser/DateParser.py
@@ -11,6 +11,10 @@ class DateParseManager (object):
  
     @staticmethod
     def RegisterParser (name, parser):
+        if not isinstance (parser, DateParser):
+        #if not issubclass (parser, DateParser)
+            raise TypeError ("%s not is DateParser!" % (parser))
+
         DateParseManager.__parserList[name] = parser
     
     @staticmethod
@@ -26,10 +30,14 @@ class DateParser (object):
     # 如果解析失败则返回None
     def Date (self, filename):
         return None
-
+    
+    # 返回支持的文件类型扩展名列表，必须是小写，扩展名需要以.开头
+    # 例如：return (".jpg", ".mkv", ".mts")
+    def FileType (self):
+        return ()
     
 
 if __name__ == "__main__":
-    DateParseManager.RegisterParser ("EXIF2", 11)
+    DateParseManager.RegisterParser ("EXIF2", DateParser())
     for k in DateParseManager.parserList():
         print ("Name:", k)
